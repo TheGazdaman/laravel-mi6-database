fdf0a8f9127b2cf352c7ea9f0d61c95d244a1171
Michal Gazdik <gazdikm@seznam.cz>
2019-11-04 15:15:32 +0100

    login form

--- resources/js/App/Components/App.jsx
+++ resources/js/App/Components/App.jsx
@@ -12,8 +12,33 @@ export default class App extends React.Component {
         }
     }
 
-    componentDidMount = () => {
+    getToken = () => {
+        return window.localStorage.getItem('_token');
+    }
 
+    setToken = (token) => {
+        window.localStorage.setItem('_token', token);
+    }
+
+    componentDidMount = () => {
+        if (null === this.getToken()) {
+            this.setState({
+                logged_in: false
+            })
+        } else {
+            this.setState({
+                logged_in: true
+            })
+        }
+    }
+
+    onLoginSuccess = (token) => {
+        this.setToken(token);
+
+        this.setState({
+            logged_in: true,
+            token: token
+        })
     }
 
     render() {
@@ -23,14 +48,14 @@ export default class App extends React.Component {
             if (this.state.logged_in) {
                 content = <PersonList />;
             } else {
-                content = <LoginForm />;
+                content = <LoginForm onLoginSuccess={ this.onLoginSuccess } />;
             }
         }
 
         return (
             <main>
                 <h1>MI6 application</h1>
-                { content }
+                 { content }
             </main>
         )
     }
